30b072b33317a9026086643991239101f3bded57
nicoconvoz <mza.nicolas.astorga@gmail.com>
2020-09-02 17:28:37 -0300

    Update pedidos-crear-usuarios.component.ts

--- src/app/components/administrador/modales/pedidos-crear-usuarios/pedidos-crear-usuarios.component.ts
+++ src/app/components/administrador/modales/pedidos-crear-usuarios/pedidos-crear-usuarios.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, Inject, Optional, ViewChild } from '@angular/core';
 import { FormGroup, Validators, FormBuilder, FormControl } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { DatePipe } from '@angular/common';
+import { UsuarioService } from 'src/app/services/allServices/usuario.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-pedidos-crear-usuarios',
@@ -21,7 +23,8 @@ export class PedidosCrearUsuariosComponent implements OnInit {
   
     constructor(public dialogRef: MatDialogRef<PedidosCrearUsuariosComponent>,
       public formBuilder: FormBuilder,
-      @Optional() @Inject(MAT_DIALOG_DATA) public data: boolean, private datePipe: DatePipe) {
+      @Optional() @Inject(MAT_DIALOG_DATA) public data: boolean, private datePipe: DatePipe,
+      private authService: UsuarioService) {
       this.dato = data;
     }
   
@@ -34,6 +37,7 @@ export class PedidosCrearUsuariosComponent implements OnInit {
     buildForm() {
       this.form = this.formBuilder.group({
         id: [this.localData.id],
+        password:[this.localData.password],
         nombre: [this.localData.nombre, [Validators.required]],
         apellido: [this.localData.apellido, [Validators.required]],
         dni: [this.localData.dni, [Validators.required, , Validators.pattern(/^[0-9]\d*$/)]],
@@ -51,8 +55,46 @@ export class PedidosCrearUsuariosComponent implements OnInit {
     }
   
     onAction() {
+      let op: boolean = this.password();
+      if(this.action=='Crear' && op){
+        this.authService.registerUser(this.form.controls['email'].value , this.form.controls['password'].value).then(
+          (res)=> {
+            this.authService.isAuth().subscribe( user =>{
+              this.form.controls['fechaNacimiento'].setValue(this.datePipe.transform(this.form.controls['fechaNacimiento'].value, 'yyyy-MM-dd'));
+              this.dialogRef.close({ event: this.action, data: this.form.value });
+            });
+          });
+            
+      }else{
+      
+      if (op) {
         this.form.controls['fechaNacimiento'].setValue(this.datePipe.transform(this.form.controls['fechaNacimiento'].value, 'yyyy-MM-dd'));
         this.dialogRef.close({ event: this.action, data: this.form.value });
+      }
+    }
+    }
+
+    public password(): boolean {
+      if (((<HTMLInputElement>document.getElementById("P1")).value === this.localData.password || this.localData.password==null)&& (<HTMLInputElement>document.getElementById("P2")).value === (<HTMLInputElement>document.getElementById("P3")).value && (<HTMLInputElement>document.getElementById("P2")).value != "" && (<HTMLInputElement>document.getElementById("P2")).value != null) {
+        this.form.controls['password'].setValue((<HTMLInputElement>document.getElementById("P2")).value);
+        return true;
+      } else {
+        if ((<HTMLInputElement>document.getElementById("P1")).value === "" || (<HTMLInputElement>document.getElementById("P1")).value === null) {
+          return true;
+        }
+      }
+      Swal.fire({
+        position: 'top-end',
+        icon: 'error',
+        title: 'Password invalida!',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      (<HTMLInputElement>document.getElementById("P1")).value = "";
+      (<HTMLInputElement>document.getElementById("P2")).value = "";
+      (<HTMLInputElement>document.getElementById("P3")).value = "";
+      (<HTMLInputElement>document.getElementById("P1")).focus();
+      return false;
     }
   
     onCancel() {
